aad01f09172cf7d0cf471786eaf00a21b3564584
Olaf Zieger <zieger@runze-casper.de>
2016-06-30 11:35:29 +0200

    Fehlerhaftes Onlinemerging korrigiert

--- index.php
+++ index.php
@@ -1,48 +1,3 @@
-<<<<<<< HEAD
-<?php  
-
-include_once 'logic.php';
-
-?><!doctype html>
-
-<html lang="de-de">
-
-<head>
-	<meta http-equiv="content-type" content="text/html; charset=utf-8">
-	<meta name="x-ua-compatible" content="IE=edge,chrome=1">
-	<title>Titel der Seite</title>
-	<link rel="stylesheet" href="css/template.css">
-	<!-- jQuery (wird für Bootstrap JavaScript-Plugins benötigt) -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-	<script src="js/bootstrap.min.js" type="text/javascript"></script>
-	<script src="js/logic.js" type="text/javascript"></script> <!--// <- use for custom script -->
-
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
-	<link rel="apple-touch-icon-precomposed" href="images/apple-touch-icon-57x57-precomposed.png">
-	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/apple-touch-icon-72x72-precomposed.png">
-	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/apple-touch-icon-114x114-precomposed.png">
-	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/apple-touch-icon-144x144-precomposed.png">
-	<!-- Le HTML5 shim and media query for IE8 support -->
-	<!--[if lt IE 9]>
-	<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
-	<script type="text/javascript" src="js/respond.min.js"></script>
-	<![endif]-->
-</head>
-  
-<body class="" role="document">
-
-	<?php require_once 'html/bootstrap.test.php'; // test only - delete this line ?>
-	<?php /*require_once 'html/bootstrap.example.php'; // test only - delete this line */?>
-
-	<!-- 
-		YOUR CODE HERE
-	-->
-
-	
-</body>
-
-</html>
-=======
 <?php
 
 include_once 'logic.php';
@@ -531,4 +486,3 @@ include_once 'logic.php';
 </body>
 
 </html>
->>>>>>> 710f7868f4a4178bcf21cbafb1c5296dbeda597d
